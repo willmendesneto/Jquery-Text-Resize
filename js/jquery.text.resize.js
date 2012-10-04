f8c65e394e06f1550973a7037a1b0c2bc61db1b4
willmendesneto <willmendesneto@gmail.com>
2012-10-04 00:50:31 -0300

    JQUERY TEXT RESIZE     	Corrigindo bug com redimensionamento de altura e largura de imagem
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

--- js/jquery.text.resize.js
+++ js/jquery.text.resize.js
@@ -1,34 +1,28 @@
 /**
  * Jquery Text Resize plugin
- *
  * @2012 Wilson Mendes ()
- *
  */
-
 /**
  * Modifica o tamanho de fonte do text; Aceita alguns parametros opcionais.
- * 
+ *
  * @example $.textResize('#the_content', { 'array_option' });
  * @desc Descreve o conteúdo de ações do plugin.
  * @example $.textResize('#the_content', { 'variation': 2, 'containerInsert':
  *          '#myContent', 'optionInterface': 'button', 'cookie': 'value_cookie'
  *          });
  * @desc Descreve o conteúdo com os valores padrão.
- * 
+ *
  * @param String
  *            com o valor referente ao nome do container ("nome do div" ou "nome
  *            da classe").
  * @param Array
  *            com os atrbutos do plugin para inicialização.
  * @type undefined
- * 
  * @name $.textResize
  * @cat Plugins/ Jquery Text Resize
  * @author Wilson Mendes/willmendesneto@gmail.com
  * @author Thiago Teles/thiagoteles.designer@gmail.com
  */
-
-/* Inicializando o plugin */
 ;
 (function($) {
 	$.textResize = function(selector, settings) {
@@ -37,10 +31,7 @@
 		if (selector) {
 			configSelector = selector;
 		}
-		/*
-		 * Array de configuracoes do Plugin Text Resize
-		 * 
-		 */
+		// Array de configuracoes do Plugin Text Resize
 		var config = {
 			'variation' : 2, 				// Valor da Variacao dos elementos
 			'animation' : false, 			// Animacao da transicao do elemento ("true" or "false")
@@ -52,9 +43,9 @@
 			'classReset' : 'resetText', 	// Nome da classe de reducao da fonte
 			'classEnlarge' : 'enlargeText', // Nome da classe de reducao da fonte
 			'clicks' : false, 				// Quantidade de clicks configurado ("false" ou inteiro referente a quantidade de clicks aceitos)
-			'containerInsert' : false, 		// Container onde vao ser inseridos os elementos ("false" ou nome do elemento onde serao inseridos os elementos 
+			'containerInsert' : false, 		// Container onde vao ser inseridos os elementos ("false" ou nome do elemento onde serao inseridos os elementos
 											//EX: "#container-test" ou ".container-test")
-			'optionInterface' : 'link', 	// Opcao de retorno de insercao dos elementos que trabalham com a fonte ("link" ou "button"; 
+			'optionInterface' : 'link', 	// Opcao de retorno de insercao dos elementos que trabalham com a fonte ("link" ou "button";
 											// Aceitos somente se a opcao "containerInsert" for diferente de "false")
 			'cookie' : false, 				// Verificacao para salvar fonte como COOKIE ("false" ou nome do elemento do array "COOKIE" configurado; Necessita do arquivo "jquery.cookie.js")
 			'fontType' : 'px'
@@ -63,35 +54,29 @@
 			$.extend(config, settings);
 		}
 
-		var obj = $(configSelector); // Iniciando o objeto com base no
-		// container informado na inicializacao
-		var img = obj.find('img'); // Achando imagem dentro do container
-		// inicializado
-		var imgDefault = img.width(); 
-		// Pegando a altura da imagem
-		var fontDefault = obj.css('font-size'); // Pegando o atributo
-												// "font-size" do container
-												// inicializado
-		var currentSize = parseInt(obj.css('font-size')); // Passando o valor
-															// "font-size" para
-															// um valor @int
-		var userClicksReduce = userClicksEnlarge = 0; // Inicializando o valor de quantidade de clicks para aumento com '0'
-		var percent = 100; // Valor para imagem
-		var fontEnlarge = ''; // Inicializando o valor de aumento com ''
-		var fontReduce = ''; // Inicializando o valor de reducao com ''
-		var cookieClicks = ''; // Valor do COOKIE
-		var value = '';
+		var obj = $(configSelector), // o objeto com base no container informado na inicializacao
+			img = obj.find('img'), // Achando imagem dentro do container inicializado
+			imgDefault = img.width(),
+			imgDefaultHeight = img.height(),
+			// Pegando a altura da imagem
+			fontDefault = obj.css('font-size'), // Pegando o atributo "font-size" do container inicializado
+			currentSize = parseInt(obj.css('font-size')), // Passando o valor "font-size" para um valor @int
+			userClicksReduce = userClicksEnlarge = 0, // Inicializando o valor de quantidade de clicks para aumento com '0'
+			percent = 100, // Valor para imagem
+			fontEnlarge = '', // Inicializando o valor de aumento com ''
+			fontReduce = '', // Inicializando o valor de reducao com ''
+			cookieClicks = '', // Valor do COOKIE
+			value = ''
+		;
 
-		// Pegando o valor do COOKIE, caso ele exista no NAVEGADOR insere o
-		// valor dele no container especificado
+		img.removeAttr('width').removeAttr('height').width(imgDefault).height(imgDefaultHeight);
+		// Pegando o valor do COOKIE, caso ele exista no NAVEGADOR insere o valor dele no container especificado
 		if ((config.cookie !== false) && (config.cookie !== 'null')) {
-
 			cookieClicks = $.cookie(config.cookie);
 			obj.css('font-size', cookieClicks + config.fontType);
 		}
 
-		// Verificacao da configuracao do container onde sera inserido os
-		// botoes/links do plugin, verificando a configuracao do plugin
+		// Verificacao da configuracao do container onde sera inserido os botoes/links do plugin, verificando a configuracao do plugin
 		if (config.containerInsert !== false) {
 			// Verificacao para a opcao de interface como LINK ou BUTTON
 			if (config.optionInterface == 'link') {
@@ -129,24 +114,22 @@
 						'font-size' : fontReduce + config.fontType
 					}, config.delay);
 					img.animate({
-						'width' : parseInt((imgDefault * percent) / 100)
+						'width' : parseInt((imgDefault * percent) / 100),
+						'height' : parseInt((imgDefaultHeight * percent) / 100)
 					}, config.delay);
 
 				} else {
 					obj.css('font-size', fontReduce);
 				}
-
 				// Incremento no contador REDUCE
 				++userClicksReduce;
 				// Decremento no contador ENLARGE
 				if (userClicksEnlarge > 0) {
 					--userClicksEnlarge;
 				}
-
 				currentSize = fontReduce;
 				// COOKIE Verification
 				if ((config.cookie != false)) {
-
 					cookieClicks = $.cookie(config.cookie);
 					cookieClicks = currentSize;
 					$.cookie(config.cookie, null);
@@ -154,13 +137,9 @@
 						path : '/',
 						expires : 5
 					});
-
 				}
-
 			}
-
 		});
-
 		// Funcao para ampliar o texto
 		$('.' + config.classEnlarge).live('click', function() {
 			if (config.clicks > userClicksEnlarge) {
@@ -172,7 +151,8 @@
 						'font-size' : fontEnlarge + config.fontType
 					}, config.delay);
 					img.animate({
-						'width' : parseInt((imgDefault * percent) / 100)
+						'width' : parseInt((imgDefault * percent) / 100),
+						'height' : parseInt((imgDefaultHeight * percent) / 100)
 					}, config.delay);
 				} else {
 					obj.css('font-size', fontEnlarge);
@@ -184,7 +164,6 @@
 					--userClicksReduce;
 				}
 				currentSize = fontEnlarge;
-
 				// COOKIE Verification
 				if ((config.cookie !== false)) {
 
@@ -195,13 +174,9 @@
 						path : '/',
 						expires : 5
 					});
-
 				}
-
 			}
-
 		});
-
 		// Funcao para retornar o texto ao tamanho padrão
 		$('.' + config.classReset).live('click', function() {
 
@@ -210,7 +185,8 @@
 					'font-size' : fontDefault
 				}, config.delay);
 				img.animate({
-					'width' : imgDefault
+					'width' : imgDefault,
+					'height' : imgDefaultHeight
 				}, config.delay);
 			} else {
 				obj.css('font-size', fontDefault);
@@ -223,14 +199,11 @@
 				cookieClicks = 'null';
 				$.cookie(config.cookie, cookieClicks);
 			}
-
 		});
-
-		// Retornando o valor para a aplicacao
 		return this;
 	}
 })(jQuery);
-/*
- * Finalizando o plugin Jquery Text Resize @name = jquery.text.resize.js @author =
- * Wilson Mendes Neto / Thiago Teles
+/**
+ * Finalizando o plugin Jquery Text Resize @name = jquery.text.resize.js
+ *  @author = Wilson Mendes Neto / Thiago Teles
  */
